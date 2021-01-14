c367a34384712e1f12b6bfe7fab8649e92e168d2
sbwml <984419930@qq.com>
2021-01-14 17:18:35 +0800

    add pixel-charger-res

--- config.mk
+++ config.mk
@@ -10,3 +10,6 @@ $(call inherit-product, $(LOCAL_PATH)/config/fonts.mk)
 
 # Bootanimation
 $(call inherit-product, $(LOCAL_PATH)/config/bootanimation.mk)
+
+# Charger-res
+$(call inherit-product, $(LOCAL_PATH)/config/pixel-charger-res.mk)

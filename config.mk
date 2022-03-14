116eec86698e402eb111fd26f244c2a695e0a51a
yehonatan2020 <yehonatan@yandex.ru>
2022-03-14 11:49:41 +0000

    fix for dot

--- config.mk
+++ config.mk
@@ -5,11 +5,6 @@ LOCAL_PATH := vendor/pixelstyle
 # Audio
 $(call inherit-product, $(LOCAL_PATH)/config/audio.mk)
 
-# Fonts
-$(call inherit-product, $(LOCAL_PATH)/config/fonts.mk)
-
 # Bootanimation
 $(call inherit-product, $(LOCAL_PATH)/config/bootanimation.mk)
 
-# Charger-res
-$(call inherit-product, $(LOCAL_PATH)/config/pixel-charger-res.mk)
--- config/pixel-charger-res.mk
+++
@@ -1,5 +0,0 @@
-PRODUCT_COPY_FILES += \
-    vendor/pixelstyle/pixel-charger-res/res/images/charger/battery_fail.png:$(TARGET_COPY_OUT_PRODUCT)/etc/res/images/charger/battery_fail.png \
-    vendor/pixelstyle/pixel-charger-res/res/images/charger/battery_scale.png:$(TARGET_COPY_OUT_PRODUCT)/etc/res/images/charger/battery_scale.png \
-    vendor/pixelstyle/pixel-charger-res/res/images/charger/main_font.png:$(TARGET_COPY_OUT_PRODUCT)/etc/res/images/charger/main_font.png \
-    vendor/pixelstyle/pixel-charger-res/res/values/charger/animation.txt:$(TARGET_COPY_OUT_PRODUCT)/etc/res/values/charger/animation.txt
